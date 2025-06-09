6d6209ece131f265316e758762f08e918a541b03
pmelo <pmelo@ciandt.com>
2025-06-09 17:29:47 -0300

    CRUD com NodeJS

---
+++ nodeJS/database-memory.js
@@ -0,0 +1,39 @@
+import { randomUUID } from "crypto"
+
+export class DatabaseMemory {
+    #videos = new Map()
+
+    list() {
+        // return this.#videos.values() -> values retorna IterableIterator
+        
+        // Vamos encapsular num Array.from que converte para um Array algo que não era array
+        // return Array.from(this.#videos.values()) 
+
+        // Ao invés de usar o values, vamos usar entries, pois traz o ID junto, mas separado das informações do vídeo
+        // map - percorrer array e fazer um tipo de modificação
+        return Array.from(this.#videos.entries()).map((videoArray) => {
+            const id = videoArray[0]
+            const data = videoArray[1]
+
+            return {
+                id,
+                ...data, // Retorna title, description e duration
+            }
+        })
+    }
+
+    create(video) {
+        // Universal Unique Id
+        const videoId = randomUUID();
+
+        this.#videos.set(videoId, video)
+    }
+
+    update(id, video) {
+        this.#videos.set(id, video)
+    }
+
+    delete(id) {
+        this.#videos.delete(id)
+    }
+}
--- nodeJS/server.js
+++ nodeJS/server.js
@@ -12,19 +12,67 @@
 // server.listen(8000)
 
 import { fastify } from 'fastify'
+import { DatabaseMemory } from './database-memory.js'
+
+// GET, POST, PUT, DELETE
 
 const server = fastify()
 
-server.get('/', () => {
-    return 'Hello World'
+const database = new DatabaseMemory()
+
+// POST http://localhost:8000/videos
+// PUT http://localhost:8000/videos/:id
+
+// Request Body
+
+server.post('/videos', (request, reply) => {
+    const { title, description, duration } = request.body
+
+    database.create({
+        title,
+        description,
+        duration,
+    })
+
+    console.log(database.list())
+
+    // status -Retorna Status HTTP como 400, 404, 200, etc...
+    // send - envia o status informado, neste caso 201 - criado com sucesso
+    return reply.status(201).send(); 
 })
 
-server.get('/hello', () => {
-    return 'Hello Rocketseat'
+server.get('/videos', () => {
+    const videos = database.list()
+
+    // return reply.status(); -> Fastify entende o return abaixo igual esse
+    return videos;
 })
 
-server.get('/node', () => {
-    return 'Hello Node'
+
+server.put('/videos/:id', (request, reply) => {
+    // dentro de params acessamos todos os parâmetros em :
+    const videoId = request.params.id;
+    const { title, description, duration } = request.body
+
+    // Parecido com POST, vai receber as informações para atualizar
+    database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    // 204 - Sucesso mas não tem conteúdo na resposta
+    return reply.status(204).send(); 
+})
+
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id;
+
+    console.log(videoId);
+    
+    database.delete(videoId);
+
+    return reply.status(204).send();
 })
 
 server.listen({
